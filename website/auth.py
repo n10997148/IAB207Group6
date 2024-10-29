626b383d829a8413dbed07fb18e33188109a575e
qnguy32 <n11270152@qut.edu.au>
2024-10-29 20:44:36 +1000

    register form

--- website/auth.py
+++ website/auth.py
@@ -8,6 +8,29 @@ from . import db
 # Create a blueprint - make sure all BPs have unique names
 auth_bp = Blueprint('auth', __name__)
 
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    register = RegisterForm()
+    #the validation of form is fine, HTTP request is POST
+    if (register.validate_on_submit()==True):
+
+        first_name = register.first_name.data
+        surname = register.surname.data
+        email = register.email.data
+        contact_number = register.contact_number.data
+        street_address = register.street_address.data
+        pwd = register.password.data
+
+        password = generate_password_hash(pwd)
+
+        new_user = User(first_name=first_name, surname=surname, email=email, password=password, contact_number=contact_number, street_address=street_address)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('main.index'))
+    else:
+        return render_template('user.html', form=register, heading='Register')
+
 # this is a hint for a login function
 @auth_bp.route('/login', methods=['GET', 'POST'])
 # view function
@@ -15,20 +38,22 @@ def login():
     login_form = LoginForm()
     error = None
     if login_form.validate_on_submit():
-        user_name = login_form.user_name.data
-        password = login_form.password.data
-        user = db.session.scalar(db.select(User).where(User.name==user_name))
+        email = login_form.email.data
+        pwd = login_form.password.data
+        user = db.session.scalar(db.select(User).where(User.email==email))
         if user is None:
-            error = 'Incorrect user name'
-        elif not check_password_hash(user.password_hash, password): # takes the hash and cleartext password
+            error = 'Incorrect email'
+        elif not check_password_hash(user.password, pwd): # takes the hash and cleartext password
             error = 'Incorrect password'
         if error is None:
             login_user(user)
-            nextp = request.args.get('next') # this gives the url from where the login page was accessed
-            print(nextp)
-            if next is None or not nextp.startswith('/'):
-                return redirect(url_for('index'))
-            return redirect(nextp)
+            return redirect(url_for('main.index'))
         else:
             flash(error)
     return render_template('user.html', form=login_form, heading='Login')
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
