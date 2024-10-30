ce333953d45ebe90f265a41e9f362c4dcfeb43a5
Bertrand Ramli <Bertrandir8899@gmail.com>
2024-10-31 01:48:50 +1000

    added search functionality

--- website/views.py
+++ website/views.py
@@ -7,7 +7,19 @@ main_bp = Blueprint('main', __name__, template_folder='templates', static_folder
 
 @main_bp.route('/')
 def index(): 
-    return render_template('index.html')
+    events = db.session.scalars(db.select(Event)).all()
+    return render_template('index.html', events=events)
+
+@main_bp.route('/search')
+def search():
+    if request.args['search'] and request.args['search'] != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        events = db.session.scalars(db.select(Event).where(Event.description.like(query)))
+        return render_template('index.html', events=events)
+    else:
+        return redirect(url_for('main.index'))
+
 
 @main_bp.route('/view_event', methods=['GET'])
 def view_event():
